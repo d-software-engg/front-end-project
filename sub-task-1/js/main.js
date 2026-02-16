53442eeaf073b31a6732b91de5edce8489bd3312
Admin <admin@macbookpro.lan>
2026-02-15 21:19:27 -0800

    TT-1: @table @style @js @fetct

--- sub-task-1/js/main.js
+++ sub-task-1/js/main.js
@@ -1,3 +1,57 @@
+var todoList;
+const startDate = new Date('2024-01-01').getTime();
+const endDate = new Date('2024-07-01').getTime();
+
+function getRandomDate(start, end) {
+    const randomTimestamp = start + Math.random() * (end - start);;
+    const date = new Date(randomTimestamp);
+    
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const year = date.getFullYear();
+    
+    return `${month}/${day}/${year}`;
+}
+
+async function getData() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=20');
+        
+        if (!response.ok) {
+            throw new Error('Error fetching data');
+        }
+
+        const data = await response.json();
+
+        todoList = data.map(d => ({
+            ...d,
+            creationDate: getRandomDate(startDate, endDate)
+        }))
+
+        console.log(todoList);
+        createTableBody(todoList);
+    } catch (error) {
+        console.error('error:', error);
+    }
+}
+
+function mapCreationDateAndBuildRow(data) {
+    return;
+}
+
+function createTableBody(data) {
+    const tableBody = document.querySelector('#tableBody');
+    const tableHTML = data.map(d => `
+        <tr>
+            <td class="item-row">${d.title}</td>
+            <td class="item-row">${d.creationDate}</td>
+        </tr>
+    `).join('');
+
+    tableBody.innerHTML = tableHTML;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('TT-1 sub-task-1 DOMContentLoaded');
+    getData();
 });
